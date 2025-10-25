3c097d3ceb1a734ccfdbb25e1a7a98507a995f83
Gustavo Henrique Silva <gohv@gft.com>
2025-10-24 22:24:18 -0300

    feat: criação do User e Task Repository

---
+++ src/main/java/com/gusta/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.gusta.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.gusta.todosimple.models.Task;
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_id(Long id);
+    
+    // @Query(value = "SELECT t FROM Task WHERE t.user.id = :id")
+    // List<Task> findByUserId(@Param("id") Long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    // List<Task> findByUserId(@Param("id") Long id);
+
+}
---
+++ src/main/java/com/gusta/todosimple/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gusta.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gusta.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
